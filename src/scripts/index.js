0887d747cca45343790bdff00178e7ae52743c3c
alia <alia.elshennawy@robustastudio.com>
2018-06-09 00:47:52 +0200

    Together section clouds

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,13 @@
+import ScrollReveal from 'scrollreveal';
+
 const App = {
-  init() {}
+  init() {
+    this.animatePages();
+  },
+  animatePages() {
+    window.sr = ScrollReveal();
+    sr.reveal('.content', { duration: 600 });
+  }
 };
 
 export default App;
